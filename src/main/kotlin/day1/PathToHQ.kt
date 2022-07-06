88e6e25db30b6ed1bd69ffaf0b7fd72cfb2c5577
Markus Spath <markus@hackr.de>
2022-07-06 09:52:11 +0200

    Day 1 pt2

--- src/main/kotlin/day1/PathToHQ.kt
+++ src/main/kotlin/day1/PathToHQ.kt
@@ -6,6 +6,7 @@ import kotlin.math.abs
 fun main() {
     val input = File("data/day1/input.txt").readText().split(", ")
     breakfast(input)
+    lunch(input)
 }
 
 data class Instruction(val direction: Char, val steps: Int)
@@ -64,3 +65,24 @@ fun breakfast(input: List<String>) {
     println(result)
 }
 
+fun lunch (input: List<String>) {
+    val directions = input.map {
+        val direction = it[0]
+        val steps = it.substring(1).toInt()
+        Instruction(direction, steps)
+    }
+    val position = Position()
+    val visited: MutableSet<Pair<Int, Int>> = mutableSetOf(Pair(0, 0))
+    var counter = 0
+    run breaking@{
+        directions.forEach { instruction ->
+            position.update(instruction)
+            val current = Pair(position.x, position.y)
+            if (visited.contains(current)) return@breaking
+            visited.add(current)
+        }
+    }
+    println(position)
+    val result = abs(position.x) + abs(position.y)
+    println(result)
+}
