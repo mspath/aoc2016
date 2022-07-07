8fa6cf8f07cd91fbfb750c0b6416313fe84cdd80
Markus Spath <markus@hackr.de>
2022-07-07 07:42:14 +0200

    Day3 pt2

--- src/main/kotlin/day3/SquaresWithThreeSides.kt
+++ src/main/kotlin/day3/SquaresWithThreeSides.kt
@@ -5,10 +5,19 @@ import java.io.File
 fun main() {
     val input = File("data/day3/input.txt").readLines()
     breakfast(input)
+    lunch(input)
 }
 
 fun breakfast(input: List<String>) {
     val triangles = input.map { it.trim().split("\\s+".toRegex()).map { n -> n.toInt() }.sorted() }
+    val result = triangles.filter { it[0] + it[1] > it[2] }.size
+    println(result)
+}
+
+fun lunch(input: List<String>) {
+    val cols = input.map { it.trim().split("\\s+".toRegex()).map { n -> n.toInt() } }
+    val values = cols.map { it[0] } + cols.map { it[1] } + cols.map { it[2] }
+    val triangles = values.windowed(3, 3).map { it.sorted() }
     val result = triangles.filter { it[0] + it[1] > it[2] }.size
     println(result)
 }
