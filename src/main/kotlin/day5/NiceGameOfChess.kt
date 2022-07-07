75f50657c2027754f6afa4d22a031e42bebaaca6
Markus Spath <markus@hackr.de>
2022-07-07 10:29:10 +0200

    Day 5 pt2

--- src/main/kotlin/day5/NiceGameOfChess.kt
+++ src/main/kotlin/day5/NiceGameOfChess.kt
@@ -4,9 +4,9 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 fun main() {
-    val sampleId = "abc"
     val doorId = "uqwqemis"
     breakfast(doorId)
+    lunch(doorId)
 }
 
 fun md5(input: String): String {
@@ -27,4 +27,23 @@ fun breakfast(doorId: String) {
         index++
     }
     println(password)
+}
+
+fun lunch(doorId: String) {
+    var index = 0
+    val password: MutableMap<Char, Char> = mutableMapOf()
+    while (true) {
+        val hash = md5(doorId + index)
+        if (hash.startsWith("00000")) {
+            val position = hash[5]
+            val value = hash[6]
+            if (position in "01234567" && !password.containsKey(position)) {
+                password[position] = value
+                if (password.size == 8) break
+            }
+        }
+        index++
+    }
+    val result = password.toSortedMap().map { it.value }.joinToString("")
+    println(result)
 }
